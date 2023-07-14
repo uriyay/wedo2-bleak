5a0e5ed89e096f78d2a3bb6bc248b3bff01ab126
uriyay <uriyay@gmail.com>
2023-07-14 03:06:06 +0300

    fixed char_read() issue by using subscribe()

--- wedo2_bleak/backends.py
+++ wedo2_bleak/backends.py
@@ -3,6 +3,11 @@ import atexit
 import time
 import bleak
 
+QUEUE_SIZE = 100
+
+class ErrorQueueFull(Exception):
+    pass
+
 def _wait(coroutine):
     loop = asyncio.get_event_loop()
     return loop.run_until_complete(coroutine)
@@ -51,12 +56,66 @@ class BleakBackend:
     def connect(self, address):
         result = BleakDevice(self, address)
         result.connect()
+        self.connected.add(result)
         return result
+    
+class CharReader:
+    def __init__(self, char_uuid):
+        self.char_uuid = char_uuid
+        #the queue must be limited by a certain size
+        self.queue = asyncio.Queue(QUEUE_SIZE)
+
+    def queue_put(self, data):
+        try:
+            self.queue.put_nowait(data)
+        except asyncio.QueueFull as e:
+            for i in QUEUE_SIZE:
+                _ = self.queue_get()
+                try:
+                    self.queue.put_nowait(data)
+                except asyncio.QueueFull:
+                    pass
+            else:
+                #something is wrong, must be a high sampling rate
+                raise ErrorQueueFull(f"when handling char uuid = {self.char_uuid}")
+
+    def queue_get(self, timeout=0.1):
+        try:
+            return _wait(asyncio.wait_for(self.queue.get(), timeout))
+        except asyncio.TimeoutError:
+            return b''
+        
+    def queue_get_latest(self, timeout=0.1):
+        '''
+        get the most latest update (based on bgapi behaviour)
+        '''
+        prev, cur = None, None
+        result = None
+        while True:
+            prev = cur
+            cur = self.queue_get(timeout)
+            if cur == b'':
+                if prev is not None:
+                    #prefer data instead of no data (which is caused by a timeout)
+                    result = prev
+                else:
+                    result = cur
+                #either way, we got to the end of the queue
+                break
+        return result
+
+    def __call__(self, handle, data):
+        if handle.uuid == self.char_uuid:
+            self.queue_put(data)
 
 class BleakDevice:
     def __init__(self, adapter, address):
         self._adapter = adapter
         self._client = bleak.BleakClient(address)
+        self.char_readers = []
+
+    def __del__(self):
+        self.disconnect()
 
     def connect(self):
         _wait(self._client.connect())
@@ -76,7 +135,20 @@ class BleakDevice:
         _wait(self._client.write_gatt_char(char_uuid, data))
 
     def char_read(self, char_uuid):
-        return _wait(self._client.read_gatt_char(char_uuid))
+        char_uuid = char_uuid.lower()
+        char_reader = None
+        for cr in self.char_readers:
+            if cr.char_uuid == char_uuid:
+                char_reader = cr
+                break
+        else:
+            #create a new one
+            char_reader = CharReader(char_uuid=char_uuid)
+            self.subscribe(char_uuid, char_reader)
+            self.char_readers.append(char_reader)
+
+        result = char_reader.queue_get_latest()
+        return result
         
     def subscribe(self, uuid, callback=None, indication=False, wait_for_response=True):
         def wrap(value_handle, data):
@@ -86,9 +158,13 @@ class BleakDevice:
         # force sleepy wait for 6 services, because wedo2's ServiceManager waits only for service 6
         # but there is another service with the number 2 that comes afterwards (also, might not receiving in the right order)
         # another problem is that ServiceManager waits by using a busy loop which affects the ability of bleak to run in parallel
-        while len(list(callback.__self__.services_data.keys())) < 6:
+        if hasattr(callback, '__self__') and hasattr(callback.__self__, 'services_data'):
+            while len(list(callback.__self__.services_data.keys())) < 6:
                 #use asyncio.sleep and not time.sleep because the second one is blocking
                 _wait(asyncio.sleep(0.1))
 
     def unsubscribe(self, uuid):
         _wait(self._client.stop_notify(uuid))
+
+    def sleep(self, interval):
+        self._adapter.pump(interval)
